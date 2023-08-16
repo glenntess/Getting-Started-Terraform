382867ec39d0939be42818da369fbbf0175299e3
Glenn Tesoriero <glenn.tesoriero@outlook.com>
2023-08-17 06:37:32 +1000

    updated files

--- globo_web_app/network.tf
+++ globo_web_app/network.tf
@@ -26,7 +26,7 @@ resource "aws_internet_gateway" "app" {
 
 resource "aws_subnet" "public_subnets" {
   count                   = var.vpc_public_subnet_count
-  cidr_block              = var.vpc_public_subnets_cidr_block[count.index]
+  cidr_block              = cidrsubnet(var.vpc_cidr_block, 8, count.index)
   vpc_id                  = aws_vpc.app.id
   map_public_ip_on_launch = var.map_public_ip_on_launch
   availability_zone       = data.aws_availability_zones.available.names[count.index]
